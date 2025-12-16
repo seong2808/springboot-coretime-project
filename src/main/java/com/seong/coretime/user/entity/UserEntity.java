ae14d0d294d355bef041e2b0806415f39453e3b1
seongkwan <seong2808@gmail.com>
2025-12-16 17:50:39 +0900

    create: UserEntity 생성

---
+++ src/main/java/com/seong/coretime/user/entity/UserEntity.java
@@ -0,0 +1,34 @@
+package com.seong.coretime.user.entity;
+
+import com.seong.coretime.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
+public class UserEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 25)
+    private String name;
+
+    @Builder
+    public UserEntity(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
