567057afb146d5b9bf7e2939e1e76d698569e237
seongkwan <seong2808@gmail.com>
2026-01-08 17:53:03 +0900

    chore: BaseEntity 생성자 수정자 추가

--- src/main/java/com/seong/coretime/common/config/JpaConfig.java
+++ src/main/java/com/seong/coretime/common/config/JpaConfig.java
@@ -1,9 +1,30 @@
 package com.seong.coretime.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            // 인증 정보가 없거나 익명 사용자일 경우 (예: 회원가입 시)
+            if (authentication == null || !authentication.isAuthenticated() ||
+                    authentication.getPrincipal().equals("anonymousUser")) {
+                return Optional.of("SYSTEM");
+            }
+
+            // 로그인한 유저의 ID(이메일 등)를 반환
+            return Optional.of(authentication.getName());
+        };
+    }
 }
--- src/main/java/com/seong/coretime/common/entity/BaseEntity.java
+++ src/main/java/com/seong/coretime/common/entity/BaseEntity.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -16,6 +18,14 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     // 생성자와 수정자 추가예정
+
+    @CreatedBy
+    @Column(updatable = false, nullable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(nullable = false)
+    private String updatedBy;
 
     @CreatedDate
     @Column(updatable = false, nullable = false)
